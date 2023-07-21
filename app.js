008b7953b9a446d864f77a52ad5790d4f45a1028
Borge <borge@milkyway.com>
2023-07-21 16:31:10 +0800

    gumana lhat

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const express = require("express");
 const session = require("express-session");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const flash = require('connect-flash')
 const app = express();
 
+// store session with sqllite3
+var SQLiteStore = require('connect-sqlite3')(session);
 
 app.use(express.json());
 
@@ -58,9 +62,10 @@ app.use((req, res, next) => {
 
 app.use(
   session({
-    resave: true,
-    saveUninitialized: true,
-    secret: "123456",
+    store: new SQLiteStore,
+    resave: false, // dating true
+    saveUninitialized: false, // dating true
+    secret: process.env.SESSION_SECRET,
     cookie: {
       maxAge: 1000 * 60 * 60,
       sameSite: "lax",
@@ -70,32 +75,197 @@ app.use(
   })
 );
 
-// let’s you use the cookieParser in your application
-// app.use(cookieParser());
+app.use(flash());
+
+// Middleware of parsing URL-encoded form data, for getting username and password
+app.use(express.urlencoded({ extended: true }))
+
+
+
+// // orig dito
+// // let’s you use the cookieParser in your application
+// // app.use(cookieParser());
+
+// app.post("/new", async (req, res) => {
+//   try {
+//     console.log('set ', req.body.name);
+//     req.session.name = req.body.name;
+//     // res.send({ message: "saved" }).status(201);
+//     res.json({ id: 1 })
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500);
+//     res.send(error);
+//   }
+// });
+
+// app.get("/name", async (req, res) => {
+//   try {
+//     // console.log('get ', req.session.name);
+//     console.log('get ', req.session.name);
+//     // res.send({ message: req.session.name });
+//     res.json({ id: req.session.name })
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500);
+//     res.send(error);
+//   }
+// });
+
+
+// // end orig dito
+
+// Middleware functions
+
+// simple middleware to test if authenticated
+// function isAuthenticated (req, res, next) {
+//   if (req.session.user) next()
+//   else next('/') // or '/login'
+// }
+
+// middleware to test if authenticated and add timestamp
+const checkIsAuthAndAddTimestamp = (req, res, next) => {
+  if (req.session.isAuth) {
+    req.session.timestamps.push(new Date().getTime())
+    next()
+  } else {
+    // res.status(403);
+    res.json({ middlewaremessage: 'unauthorized access'});
+  }
+}
+
+// orig
 
 app.post("/new", async (req, res) => {
   try {
-    console.log('set ', req.body.name);
-    req.session.name = req.body.name;
-    // res.send({ message: "saved" }).status(201);
-    res.json({ id: 1 })
+    // Use this for Wrong username or password
+    // if (!targetUser) {
+    //   req.flash('showInputError', true)
+    //   return res.json('MALI!')
+    // }
+
+    // res.clearCookie('connect.sid');
+
+    req.session.isAuth = true;
+    // in prod use data from mongo user.username value
+    req.session.userName = req.body.name;
+    req.session.timestamps = [];
+    // in prod use data from mongo user object except -password
+    req.session.user = {email: 'a@a.com', username: req.body.name, role: 'Chef'};
+
+    const response = {
+      isAuth: req.session.isAuth,
+      userName : req.session.user.username,
+      user: req.session.user
+    };
+
+      // Setting cookie (key-value)
+    res.cookie('title', 'geeksforgeeks');
+
+    console.log('get ', response);
+    res.send(response);
   } catch (error) {
     console.log(error);
-    res.status(500);
+    // res.status(404);
     res.send(error);
   }
 });
 
-app.get("/name", async (req, res) => {
+app.get("/name", checkIsAuthAndAddTimestamp, async (req, res) => {
   try {
-    // console.log('get ', req.session.name);
-    console.log('get ', req.session.name);
-    // res.send({ message: req.session.name });
-    res.json({ id: req.session.name })
+    const { isAuth, user, userName, timestamps } = req.session;
+    const response = {
+      isAuth: isAuth,
+      userName : userName,
+      user: user,
+      timestamps: timestamps
+    };
+    console.log('get ', response);
+    res.send(response);
   } catch (error) {
     console.log(error);
-    res.status(500);
+    // res.status(403);
     res.send(error);
   }
 });
+
+// end orig
+
+// app.post('/logout', async (req, res)  => {
+
+//   // // req.session.destroy();
+
+//   // // delete req.session;
+//   // req.session.isAuth = null;
+//   // // in prod use data from mongo user.username value
+//   // req.session.userName = null;
+//   // req.session.timestamps = null;
+//   // // in prod use data from mongo user object except -password
+//   // req.session.user = null;
+
+
+//   // req.session.destroy((err) => {
+//   //   if (err) {
+//   //     // res.status(404);
+//   //     return res.send({ logout_error : err });
+//   //   }
+//   //   req.session = null;
+//   // })
+
+//   // // Default name of cookie set by express-session
+//   // res.clearCookie('connect.sid', { path: '/' });
+//   // // res.cookie('connect.sid', '', {maxAge: 0, domain:'localhost', path:'/'});
+//   // res.cookie = '';
+//   // req.cookies = '';
+
+//   req.session.destroy();
+
+//   res.clearCookie('connect.sid', { path: '/' });
+//   res.send({logout: 'success'});
+
+//   // res.end();
+// });
+
+
+// gumana ayup!!!!!!!!!! get lang ang katapat
+// dapat wag mo na pala i destroy ang session
+// cookie lang okay na!!!!!!
+app.get('/logout', function (req, res) {
+
+  // Clearing the cookie
+  res.clearCookie('title');
+  res.clearCookie('connect.sid');
+
+  console.log("Cookie cleared");
+  res.send('logout success!');
+  res.end();
+});
+
+// app.post('/logout', async function (req, res, next) {
+//   // logout logic
+
+//   // clear the user from the session object and save.
+//   // this will ensure that re-using the old session id
+//   // does not have a logged in user
+//   req.session.user = null
+//   res.clearCookie('connect.sid', { path: '/' });
+//   req.session.save(function (err) {
+//     if (err) next(err)
+
+//     // regenerate the session, which is good practice to help
+//     // guard against forms of session fixation
+//     req.session.regenerate(function (err) {
+//       if (err) next(err)
+//       // res.redirect('/')
+//       res.send('tae')
+//     })
+//   })
+// })
+
+
+
+
+// end from source
+
+
 app.listen(PORT, () => console.log(`SERVER IS RUNNING ON ${PORT}`));
---
+++ source_app.js
@@ -0,0 +1,124 @@
+require('dotenv').config()
+const express = require('express')
+const flash = require('connect-flash')
+const app = express()
+const port = 3000
+app.set('view engine', 'ejs')
+
+// with memorystore
+// const session = require('express-session')
+// const MemoryStore = require('memorystore')(session)
+
+
+// with sqllite3
+var session = require('express-session');
+var SQLiteStore = require('connect-sqlite3')(session);
+
+
+// with redis
+// const redis = require('redis')
+// var session = require('express-session')
+
+// let RedisStore = require('connect-redis')(session)
+// let redisClient = redis.createClient()
+
+// redisClient.on('error', (err) =>
+//   console.log(`Fail to connect to redis. ${err}`)
+// )
+// redisClient.on('connect', () => console.log('Successfully connect to redis'))
+
+const users = [
+  { id: 111, username: 'tom', password: '123' },
+  { id: 222, username: 'chris', password: '123' },
+  { id: 333, username: 'mary', password: '123' },
+]
+
+// app.use(
+//   session({
+//     store: new RedisStore({ client: redisClient }),
+//     secret: process.env.SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//   })
+// )
+
+app.use(
+  session({
+    store: new SQLiteStore, // with sqllite3
+    // store: new MemoryStore({
+    //     checkPeriod: 86400000 // prune expired entries every 24h
+    //   }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+)
+
+app.use(flash())
+
+app.get('/', (req, res) => {
+  if (req.session.isAuth) {
+    return res.redirect('/profile')
+  }
+  const showInputError = req.flash('showInputError')[0] || false
+  res.render('index', { showInputError })
+})
+
+// Middleware of parsing URL-encoded form data, for getting username and password
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/', (req, res) => {
+  const { username, password } = req.body
+
+  // Prevent empty input
+  if (username.trim() === '' || password.trim() === '') {
+    req.flash('showInputError', true)
+    return res.redirect('/')
+  }
+
+  // Validate input
+  const targetUser = users.find(
+    (user) => user.username === username && user.password === password
+  )
+
+  // Wrong username or password
+  if (!targetUser) {
+    req.flash('showInputError', true)
+    return res.redirect('/')
+  }
+
+  req.session.isAuth = true
+  req.session.username = targetUser.username
+  req.session.timestamps = []
+  res.redirect('/profile')
+})
+
+// Middleware function
+const checkIsAuthAndAddTimestamp = (req, res, next) => {
+  if (req.session.isAuth) {
+    req.session.timestamps.push(new Date().getTime())
+    next()
+  } else {
+    res.redirect('/')
+  }
+}
+
+app.get('/profile', checkIsAuthAndAddTimestamp, (req, res) => {
+  const { username, timestamps } = req.session
+  res.render('profile', { username, timestamps })
+})
+
+app.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.redirect('/profile')
+    }
+  })
+  // Default name of cookie set by express-session
+  res.clearCookie('connect.sid')
+  res.redirect('/')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
